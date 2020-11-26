9e4c0a7d291225d87a1198f60643089f6ac60a61
MolnarAttila <MolnarAttila@tan-nt1.tanari.szlgbp.local>
2020-11-26 14:54:38 +0100

    models.py-ba deklaráltuk a dolgokat

--- feladatkiosztoprojekt/kiosztapp/models.py
+++ feladatkiosztoprojekt/kiosztapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Tetel(models.Model):
+	"""docstring for Tetel"""
+	def __init__(self, arg):
+		super(Tetel, self).__init__()
+		self.arg = arg
+	
+	tenev = models.CharField(max_length=100)
+
+class Tanulo(models.Model):
+	"""docstring for Tanulo"""
+	def __init__(self, arg):
+		super(Tanulo, self).__init__()
+		self.arg = arg
+	tanev = models.CharField(max_length=100)
+	jelszo = models.CharField(max_length=20)
+
+
+class Valasztott(models.Model):
+	"""docstring for Valasztott"""
+	def __init__(self, arg):
+		super(Valasztott, self).__init__()
+		self.arg = arg
+	tetelid = models.ForeignKey(Tetel, on_delete=models.CASCADE, blank = True, null = True)
+	tanuloid = models.ForeignKey(Tanulo, on_delete=models.CASCADE, blank = True, null = True)
