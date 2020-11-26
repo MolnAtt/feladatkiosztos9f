ae9a7d96f2748b82e70457cac8704c637fcd6dab
MolnarAttila <MolnarAttila@tan-nt1.tanari.szlgbp.local>
2020-11-26 14:13:43 +0100

    view létrehozva

--- feladatkiosztoprojekt/kiosztapp/views.py
+++ feladatkiosztoprojekt/kiosztapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_view(request, *args, **kwargs):
+	kontextus = {"w":5}
+	return render(request, "kioszthtml.html", kontextus) # latter is the context
