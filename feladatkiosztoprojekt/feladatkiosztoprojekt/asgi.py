a0be50b6822c3be86d168dc7f00408cae3d158b2
MolnarAttila <MolnarAttila@tan-nt1.tanari.szlgbp.local>
2020-11-26 13:46:05 +0100

    megcsináltam a projektet

---
+++ feladatkiosztoprojekt/feladatkiosztoprojekt/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for feladatkiosztoprojekt project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feladatkiosztoprojekt.settings')
+
+application = get_asgi_application()
---
+++ feladatkiosztoprojekt/feladatkiosztoprojekt/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for feladatkiosztoprojekt project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feladatkiosztoprojekt.settings')
+
+application = get_wsgi_application()
