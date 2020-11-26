752f8ca4a70f7db1dd00a7c5e37ba2d1d602c16c
MolnarAttila <MolnarAttila@tan-nt1.tanari.szlgbp.local>
2020-11-26 15:13:10 +0100

    admin-site-on regisztráltuk a modelleket

--- feladatkiosztoprojekt/kiosztapp/admin.py
+++ feladatkiosztoprojekt/kiosztapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tanulo
+from .models import Tetel
+from .models import Valasztott
+
+admin.site.register(Tanulo)
+admin.site.register(Tetel)
+admin.site.register(Valasztott)
 
 # Register your models here.
