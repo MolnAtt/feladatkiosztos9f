733db5d764c617491dcd4753e29071ce8b0c9882
MolnarAttila <MolnarAttila@tan-nt1.tanari.szlgbp.local>
2020-12-03 14:36:17 +0100

    kiírtuk, hogy ki mit vállal (gombozni még nem lehet)

--- feladatkiosztoprojekt/kiosztapp/views.py
+++ feladatkiosztoprojekt/kiosztapp/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 
+from .models import Tetel, Tanulo, Valasztott;
+
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
-	kontextus = {"lista":
+	print('--------------- views.home_view -----------------')
+
+	lista = []
+
+	for t in Tetel.objects.all():
+		szures = Valasztott.objects.filter(tetelid = t)
+		if len(szures)>0:
+			lista.append([t.tenev, szures[0].tanuloid.tanev])
+		else:
+			lista.append([t.tenev, "szabadon választható"])
+		print(f'{t.tenev} tetelnév hozzáadva a lista listához')
+	"""	kontextus = {"lista":
 		[
 			["1. Tétel", "Fodor Dániel"], 
 			["2. Tétel", "üres"]
 		] 
-		}
+		} """
+	kontextus = {"lista": lista}
 	return render(request, "kioszthtml.html", kontextus) # latter is the context
