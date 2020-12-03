1c8a8c600bcf03f97c599d876653865c1e9a9290
MolnarAttila <MolnarAttila@tan-nt1.tanari.szlgbp.local>
2020-12-03 13:37:24 +0100

    views.py-ban létrehoztunk egy listát és rendereltük a template-be

--- feladatkiosztoprojekt/kiosztapp/views.py
+++ feladatkiosztoprojekt/kiosztapp/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
-	kontextus = {"w":5}
+	kontextus = {"lista":
+		[
+			["1. Tétel", "Fodor Dániel"], 
+			["2. Tétel", "üres"]
+		] 
+		}
 	return render(request, "kioszthtml.html", kontextus) # latter is the context
