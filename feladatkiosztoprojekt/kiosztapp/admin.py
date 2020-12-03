d067508a6318652444453a170dceea86d3590441
MolnarAttila <MolnarAttila@tan-nt1.tanari.szlgbp.local>
2020-12-03 12:58:09 +0100

    admin.py-ban regisztráltuk a dolgokat a múltkor

--- feladatkiosztoprojekt/kiosztapp/admin.py
+++ feladatkiosztoprojekt/kiosztapp/admin.py
@@ -3,8 +3,8 @@ from .models import Tanulo
 from .models import Tetel
 from .models import Valasztott
 
+# Register your models here.
+
 admin.site.register(Tanulo)
 admin.site.register(Tetel)
 admin.site.register(Valasztott)
-
-# Register your models here.
