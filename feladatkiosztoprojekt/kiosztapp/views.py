5a50b1ef4b5b614515274ef5d2ddd7c173c648d1
MolnarAttila <MolnarAttila@tan-nt1.tanari.szlgbp.local>
2020-12-10 14:41:56 +0100

    Lehet jelölni!

--- feladatkiosztoprojekt/kiosztapp/views.py
+++ feladatkiosztoprojekt/kiosztapp/views.py
@@ -7,6 +7,29 @@ from .models import Tetel, Tanulo, Valasztott;
 def home_view(request, *args, **kwargs):
 	print('--------------- views.home_view -----------------')
 
+	if request.method == "POST":
+		print("ez egy post request")
+
+		valaszto_tanulo = ""
+		valasztott_tetel = ""
+
+		megjeloltnevutanulok = Tanulo.objects.filter(tanev=request.POST['user'])
+		if len(megjeloltnevutanulok)>0 and megjeloltnevutanulok[0].jelszo == request.POST['pwd']:
+			valaszto_tanulo = megjeloltnevutanulok[0]
+
+			print("valóban van ilyen felhasználó")
+
+			print(f'Ezt választotta: {list(request.POST.keys())[3]}')
+			valasztott_tetelnev = list(request.POST.keys())[3]
+			valasztott_tetel = Tetel.objects.get(tenev = valasztott_tetelnev)
+
+			Valasztott.objects.create(tanuloid = valaszto_tanulo, tetelid = valasztott_tetel)
+
+
+		else:
+			print("nincs ilyen felhasználó vagy van felhasználó, de nem ez a jelszava")
+
+
 	lista = []
 
 	for t in Tetel.objects.all():
@@ -14,8 +37,9 @@ def home_view(request, *args, **kwargs):
 		if len(szures)>0:
 			lista.append([t.tenev, szures[0].tanuloid.tanev])
 		else:
-			lista.append([t.tenev, "szabadon választható"])
+			lista.append([t.tenev, "nincs"])
 		print(f'{t.tenev} tetelnév hozzáadva a lista listához')
 		
 	kontextus = {"lista": lista}
+
 	return render(request, "kioszthtml.html", kontextus) # latter is the context
